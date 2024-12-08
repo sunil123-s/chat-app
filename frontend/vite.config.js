d5a7f69ee88e0b62499c5bb861812484090955de
Sunil kumar <sunil1234kumar567@gmail.com>
2024-12-08 15:27:11 +0530

    fffff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root:"/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
