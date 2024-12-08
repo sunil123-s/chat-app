b317cd77272de3b046715646b6a20041cb39e147
Sunil kumar <sunil1234kumar567@gmail.com>
2024-12-08 20:11:06 +0530

    dfdfdf

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,10 +34,10 @@ const uploadDir = path.resolve()
 
  if(process.env.NODE_ENV === "production") {
   //  app.use(express.static(path.join(uploadDir, "/frontend/dist")))
-   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+   app.use(express.static(path.join(__dirname, "public")));
    app.get("*",(req,res) => {
     // res.sendFile(path.resolve(uploadDir,"frontend", "dist", "index.html"))
-    res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
    })
  }
 
